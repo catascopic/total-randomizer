c321aca21f2a5f5cef707033b83300c830d31683
Max Koren <Koren_Max@bah.com>
2020-10-21 16:25:09 -0400

    why is this so hard

--- script.js
+++ script.js
@@ -12,11 +12,13 @@ var piles;
 var setupHistory;
 var currentSetup;
 
-function init(initDominion) {
-	dominion = initDominion;
+function loadCards(json) {
+	dominion = json;
+}
+
+function setupState(initialPiles) {
 	
 	let rawState = localStorage.getItem('state');
-
 	let state;
 	if (rawState) {
 		try {
@@ -31,55 +33,95 @@ function init(initDominion) {
 	
 	if (!state) {
 		active = {'Base': true};
-		piles = newState();
+		piles = newPiles(initialPiles);
 		setupHistory = [];
 		saveState();
 	} else {
 		active = state.active;
-		piles = state.piles;
+		piles = loadPiles(state.piles);
 		setupHistory = state.history;
 	}
 }
 
-function newState() {
-	piles = [];
-	sets = {};
-	
-	for (let card of Object.values(dominion)) {
-		if (card.set = 'Promo') {
-			piles.push(newPromo(card));
-		} else {
-		
-		}
+
+
+function push(obj, key, item) {
+	let array = obj[key];
+	if (!array) {
+		array = [];
+		obj[key] = array;
 	}
+	array.push(item);
 }
 
-function newExpansion(expansion, lastIndex = -1) {
-	let cards = expansion.cards;
-	let index = lastIndex;
+function createPile(_cards, _used) {
+
+	let cards = _cards;
+	let used = _used;
+	let current = [];
+	let recycle = 0;
+	let size = cards.length;
+
 	return {
-		size: cards.length,
-		isActive: function() {
-			return isActive(expansion);
+		size: function() {
+			return size - current.length;
 		},
-		get: function() {
-			index = (index + 1) % cards.length;
-			if (!index) {
+		draw: function() {
+			if (!cards.length) {
+				cards = used;
+				used = [];
 				shuffle(cards);
+				// the current cards missed the shuffle, so mark their position so we can shuffle them back in
+				recycle = current.length;
 			}
-			return cards[index];
+			let card = cards.pop();
+			current.push(card);
+			return card;
+		},
+		finish: function() {
+			let i;
+			for (i = 0; i < recycle; i++) {
+				randomInsert(cards, current[i]);
+			}
+			for (; i < current.length; i++) {
+				used.push(current[i]);
+			}
+			current = [];
+			recycle = 0;
 		}
 	};
 }
 
-function newPromo(promo) {
+// promos should rotate too!
+
+/*
+I was thinking about having one big stack, just skip the cards that aren't active. This sounded fine until I realized you might keep skipping the same cards while the set was inactive.  In that case, the pile method is better, but there are still two problems: promo distribution, and to a lesser extent, fairness in selecting the expansions.
+
+Okay, what if I use the "one big stack" algorithm, but instead of cards being in the stack, there were "vouchers" that allowed drawing from the pile?
+
+That really only makes the pile selection more fair, though.  Which is good, but that wasn't my biggest problem.  Although who knows, maybe my weird totalling-and-randomizing algorithm wasn't great anyway.  Draw until you hit a valid voucher probably isn't any worse, and it sounds easier to follow, at least.
+
+Sideways cards are another issue, but because there are only 2 I can probably get away with pure randomness.  Although again, Ways make it a little more complicated.
+
+Also, even with the voucher system, it's theoretically possible that you could overdraw from a pile.  Do we remove the vouchers while we shuffle?  If so, do we shuffle them back in when we're done, or put them in the "used" pile?  I think with vouchers it seems fine to put them back.
+
+*/
+
+
+function createSingleCardPile(promo) {
+
+	let ready = true;
+
 	return {
-		size: 1,
-		isActive: function() {
-			return isActive(promo);
+		size: function() {
+			return Number(ready);
 		},
-		get: function() {
+		draw: function() {
+			ready = false;
 			return promo;
+		},
+		finish: function() {
+			ready = true;
 		}
 	};
 }
@@ -104,10 +146,10 @@ function generate() {
 	let chosenCards = 0;
 	while (chosenCards < KINGDOM_SIZE) {
 		let activePiles = piles.filter(isActive);
-		let total = activePiles.reduce((running, pile) => running + pile.size, 0);
+		let total = activePiles.reduce((running, pile) => running + pile.size(), 0);
 		let index = randInt(0, total);
 		for (let pile of activePiles) {
-			if (index < pile.size) {
+			if (index < pile.size()) {
 				let item = getItem(pile);
 				updateDisplay(item);
 				if (item.landscape) {
@@ -164,7 +206,7 @@ function updateDisplay(card) {
 }
 
 function finishDisplay(card) {
-	currentSetup.sort(cardComparator);
+	currentSetup.sort(compareCard);
 	setupHistory.push({
 		setup: currentSetup,
 		date: new Date().toString()
@@ -175,33 +217,42 @@ function finishDisplay(card) {
 	}
 }
 
-function cardComparator(c1, c2) {
-	let landscapeCmp = Number(Boolean(c1.landscape)) - Number(Boolean(c2.landscape));
-	if (landscapeCmp != 0) {
-		return landscapeCmp;
+function compareCard(c1, c2) {
+	let landscapeSign = compareTruthy(c1.landscape, c2.landscape);
+	if (landscapeSign != 0) {
+		return landscapeSign;
 	}
-	let costCmp = (c1.coins || 0) - (c2.coins || 0);
-	if (costCmp != 0) {
-		return costCmp;
+	let coinsSign = (c1.coins || 0) - (c2.coins || 0);
+	if (coinsSign != 0) {
+		return coinsSign;
 	}
 	return c1.name.localeCompare(c2.name);
 }
 
-function randomInsert(pile, thing) {
-	let index = randInt(0, pile.length + 1);
-	// if index == length, we push undefined here, which is fine
-	pile.push(pile[index]);
-	pile[index] = thing;
+function compareTruthy(t1, t2) {
+	return Number(Boolean(t1)) - Number(Boolean(t2));
 }
 
-function shuffle(array) {
-	for (let i = array.length; i > 1; i--) {
-		swap(array, i - 1, randInt(0, i));
+// assumes the array is already shuffled
+function randomInsert(array, item) {
+	let index = randInt(0, array.length + 1);
+	// if index == length, we push undefined here, which is fine
+	array.push(array[index]);
+	array[index] = item;
+}
+
+function shuffleRange(array, start, end) {
+	for (let i = end - 1; i > start; i--) {
+		swap(array, i, randInt(start, i + 1));
 	}
 }
 
-function randInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+function shuffle(array) {
+	shuffleRange(array, 0, array.length);
+}
+
+function randInt(origin, bound) {
+    return Math.floor(Math.random() * (bound - origin)) + origin;
 }
 
 function swap(array, i, j) {
